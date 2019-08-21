33235ca6be246c1e207a255b5d9e6750337bfe70
kitty <hello@.com>
2019-08-22 00:16:44 +0800

    Spring Cloud开发价格服务

---
+++ src/main/java/com/roncoo/eshop/price/model/ProductPrice.java
@@ -0,0 +1,43 @@
+package com.roncoo.eshop.price.model;
+
+public class ProductPrice {
+
+	private Long id;
+	private Double value;
+	private Long productId;
+	
+	
+	
+	
+	
+	
+	public ProductPrice() {
+		super();
+	}
+	public ProductPrice(Long id, Double value, Long productId) {
+		super();
+		this.id = id;
+		this.value = value;
+		this.productId = productId;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Double getValue() {
+		return value;
+	}
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	public Long getProductId() {
+		return productId;
+	}
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	
+	
+}
